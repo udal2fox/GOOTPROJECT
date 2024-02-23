2050c425ffc3663a74f33243818ab48492ce3fb9
freetebet <id147147@naver.com>
2024-02-23 18:54:56 +0900

    change

--- src/main/java/org/rainbow/controller/DefaultController.java
+++ src/main/java/org/rainbow/controller/DefaultController.java
@@ -10,10 +10,7 @@ public class DefaultController
 	public String testMapping()
 	{
 		// return "테스트시 DefaultController 사용 가능!!";
-<<<<<<< HEAD
+
 		 return "/employeeSupervisePage/profile_modify";
-=======
-		 return "/userAdminPage/userLogin";
->>>>>>> 8987019f68398e447010e68721de135df17fdfbd
 	}
 }

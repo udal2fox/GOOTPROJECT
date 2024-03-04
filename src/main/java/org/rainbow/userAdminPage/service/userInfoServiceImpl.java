1b28c6c16cdea51cff5e8791517df8411bdb2619
NINI <wns1923@naver.com>
2024-03-04 20:37:52 +0900

    서비스 수정

--- src/main/java/org/rainbow/userAdminPage/service/userInfoServiceImpl.java
+++ src/main/java/org/rainbow/userAdminPage/service/userInfoServiceImpl.java
@@ -3,10 +3,12 @@ package org.rainbow.userAdminPage.service;
 import lombok.extern.log4j.Log4j;
 import org.rainbow.userAdminPage.mapper.userInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Log4j
+@Service
 public class userInfoServiceImpl implements userInfoService{
 
     @Autowired

a1f7755716f85ba001add8f1fd53da52985d4048
NINI <wns1923@naver.com>
2024-03-04 11:36:57 +0900

    2페이지

--- src/main/java/org/rainbow/company/custMgmt/service/salesServiceImpl.java
+++ src/main/java/org/rainbow/company/custMgmt/service/salesServiceImpl.java
@@ -2,7 +2,6 @@ package org.rainbow.company.custMgmt.service;
 
 import java.util.List;
 
-
 import org.rainbow.company.custMgmt.domain.consultVO;
 import org.rainbow.company.custMgmt.mapper.salesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,35 +12,27 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Service
 public class salesServiceImpl implements salesService {
-	
-	
-	
-	  @Autowired
-	   private salesMapper salesMapper;
-	 
 
-	/** 'salesList.jsp' 에서 상담 요청 리스트 가져오기  */
+	@Autowired
+	private salesMapper salesMapper;
+
 	@Override
 	public List<consultVO> salesList() {
-		log.info("getSalesList....");
+		
 		return salesMapper.salesList();
 	}
-	
-	
-	
-	/** 'salesView.jsp' 에서 상담 신청 내용 가져오기  */
 	@Override
-	public consultVO getSalesView(int consultNo) {
-		log.info("getSalesView...." + consultNo);
-		return salesMapper.getSalesView(consultNo);
+	public consultVO salesView(int consultNo) {
+		
+		return salesMapper.salesView(consultNo);
 	}
 	
-	/** 'salesView.jsp' 에서 영업 내용 저장하기 */
 	@Override
 	public int saveSales(consultVO vo) {
-		log.info("saveSales...." + vo);
+		
 		return salesMapper.saveSales(vo);
 	}
-	
+
+
 
 }

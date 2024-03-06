897a6d6b91dcd3c8c39eae7568882fd1eaf62adf
NINI <wns1923@naver.com>
2024-03-06 12:03:01 +0900

    js계산식수정

--- src/main/webapp/resources/js/company/productPage/prdInput.js
+++ src/main/webapp/resources/js/company/productPage/prdInput.js
@@ -9,42 +9,41 @@
 	let salSub = document.querySelector('.salSub');
 	
 	cost.forEach(function(e) {
-	  e.addEventListener('change', function() {
-	    let criValue = parseFloat(cri.value);
-	    let salValue = parseFloat(sal.value);
-	
-	    if (isNaN(criValue) || isNaN(salValue) || criValue < 0 || salValue < 0) {
-	      magin.value = 0;
-	      critax.value = 0;
-	      saltax.value = 0;
-	      criSub.value = 0;
-	      salSub.value = 0;
-	    } else {
-	      if (salValue <= 0) {
-	        magin.value = 0;
-	        critax.value = 0;
-	        saltax.value = 0;
-	        criSub.value = 0;
-	        salSub.value = 0;
-	      } else {
-
-	        magin.value = (((salValue - criValue) / salValue)).toFixed(2);
-	        
-	        // 과세 여부에 따라 세액을 계산하거나 0으로 설정
-	        if (document.querySelector('#exampleRadios1').checked) {
-	          critax.value = (criValue * 0.1).toFixed(2);
-	          saltax.value = (salValue * 0.1).toFixed(2);
-	        } else {
-	          critax.value = 0;
-	          saltax.value = 0;
-	        }
-	        
-	        criSub.value = (criValue + parseFloat(critax.value)).toFixed(2);
-	        salSub.value = (salValue - parseFloat(saltax.value)).toFixed(2);
-	      }
-	    }
-	  });
-	});
+		  e.addEventListener('change', function() {
+		    let criValue = parseFloat(cri.value);
+		    let salValue = parseFloat(sal.value);
+		
+		    if (isNaN(criValue) || isNaN(salValue) || criValue < 0 || salValue < 0) {
+		      magin.value = 0;
+		      critax.value = 0;
+		      saltax.value = 0;
+		      criSub.value = 0;
+		      salSub.value = 0;
+		    } else {
+		      if (salValue <= 0) {
+		        magin.value = 0;
+		        critax.value = 0;
+		        saltax.value = 0;
+		        criSub.value = 0;
+		        salSub.value = 0;
+		      } else {
+		        magin.value = (((salValue - criValue) / salValue)).toFixed(2);
+		        
+		        // 과세 여부에 따라 세액을 계산하거나 0으로 설정
+		        if (document.querySelector('#exampleRadios1').checked) {
+		          critax.value = Math.floor(criValue * 0.1);
+		          saltax.value = Math.floor(salValue * 0.1);
+		        } else {
+		          critax.value = 0;
+		          saltax.value = 0;
+		        }
+		        
+		        criSub.value = (criValue + parseFloat(critax.value))
+		        salSub.value = (salValue - parseFloat(saltax.value))
+		      }
+		    }
+		  });
+		});
 	
 	// 라디오버튼 이벤트 과세시 세율 활성 비과세시 비활성화 기능
 	document.querySelectorAll('[name="prdTaxExSt"]').forEach(function(radio) {
@@ -53,12 +52,22 @@
 	      console.log("과세");
 	      document.querySelectorAll(".tax").forEach(function(e) {
 	        e.disabled = false;
+	        // 체인지 이벤트를 직접 부여
+	       cost.forEach(function(e) {
+	            let event = new Event('change');
+	            e.dispatchEvent(event);
+	          });
 	      });
 	    } else {
 	      console.log("비과세");
 	      document.querySelectorAll(".tax").forEach(function(e) {
 	        e.disabled = true;
 	        e.value = 0; // 비과세일 때 세액 필드를 0으로 설정
+	        // 체인지 이벤트를 직접 부여
+		       cost.forEach(function(e) {
+		            let event = new Event('change');
+		            e.dispatchEvent(event);
+		       });
 	      });
 	    }
 	  });
--- src/main/webapp/resources/js/company/productPage/prdUpdate.js
+++ src/main/webapp/resources/js/company/productPage/prdUpdate.js
@@ -10,8 +10,7 @@
 	let criSub = document.querySelector('.criSub');
 	let salSub = document.querySelector('.salSub');
 
-	function prdCalculation() 
-	{
+	
 		cost.forEach(function(e) {
 		  e.addEventListener('change', function() {
 		    let criValue = parseFloat(cri.value);
@@ -35,28 +34,19 @@
 		        
 		        // 과세 여부에 따라 세액을 계산하거나 0으로 설정
 		        if (document.querySelector('#exampleRadios1').checked) {
-		          critax.value = (criValue * 0.1).toFixed(2);
-		          saltax.value = (salValue * 0.1).toFixed(2);
+		          critax.value = Math.floor(criValue * 0.1);
+		          saltax.value = Math.floor(salValue * 0.1);
 		        } else {
 		          critax.value = 0;
 		          saltax.value = 0;
 		        }
 		        
-		        criSub.value = (criValue + parseFloat(critax.value)).toFixed(2);
-		        salSub.value = (salValue - parseFloat(saltax.value)).toFixed(2);
+		        criSub.value = (criValue + parseFloat(critax.value))
+		        salSub.value = (salValue - parseFloat(saltax.value))
 		      }
 		    }
 		  });
 		});
-	}
-
-
-
-
-
-
-
-
 
 
 	// 라디오버튼 이벤트 과세시 세율 활성 비과세시 비활성화 기능
@@ -66,13 +56,22 @@
 	      console.log("과세");
 	      document.querySelectorAll(".tax").forEach(function(e) {
 	        e.disabled = false;
-	        prdCalculation();
+	        // 체인지 이벤트를 직접 부여
+	       cost.forEach(function(e) {
+	            let event = new Event('change');
+	            e.dispatchEvent(event);
+	          });
 	      });
 	    } else {
 	      console.log("비과세");
 	      document.querySelectorAll(".tax").forEach(function(e) {
 	        e.disabled = true;
 	        e.value = 0; // 비과세일 때 세액 필드를 0으로 설정
+	        // 체인지 이벤트를 직접 부여
+		       cost.forEach(function(e) {
+		            let event = new Event('change');
+		            e.dispatchEvent(event);
+		          });
 	      });
 	    }
 	  });

f9e7b75c32a37f3dbbb11ac4bba73b51ec0b602c
NINI <wns1923@naver.com>
2024-03-22 13:09:36 +0900

    0309pm

--- src/main/webapp/resources/js/company/productPage/supsUpDownLoad.js
+++ src/main/webapp/resources/js/company/productPage/supsUpDownLoad.js
@@ -5,6 +5,12 @@ document.getElementById("uploadBtn").addEventListener("click", function() {
 
 // 선택된 파일 이름 표시
 document.getElementById("excelUpload").addEventListener("change", function() {
+	
+	if(deptNo != 0 && deptNo != 4)
+	{
+		alert("불허된 접근입니다.")
+		return;
+	}
 	let input = this;
 	let files = input.files;
 	let fileName = input.files[0].name;
@@ -60,6 +66,11 @@ document.getElementById('downloadButton').addEventListener('click', ()=>{
 	download();
 });
 
+//다운로드 버튼 눌러서 함수호출
+document.getElementById('exExcelSups').addEventListener('click', ()=>{
+	exSupsDownload();
+});
+
 
 
 function download() 
@@ -82,9 +93,6 @@ function download()
          	checkedValues.push(checkbox.value);
  		}
     });
-
-	
-	
 	
     // 서버로 데이터 전송
     fetch('/supsExcelDown', {
@@ -110,4 +118,27 @@ function download()
         console.error('Error:', error);
     });
 }
+
+function exSupsDownload() 
+{
+	fetch('/exSupsExcel')
+    .then(response => response.blob())
+    .then(blob =>
+    {
+        // 엑셀 파일 다운로드	
+        const url = window.URL.createObjectURL(new Blob([blob])); 	// Blob 데이터로부터 URL 생성
+        const a = document.createElement('a');  					// <a> 요소 생성
+        a.href = url;  
+        a.download = 'exExcel.xlsx';  							// 다운로드되는 파일의 이름 설정
+        document.body.appendChild(a); 								// <a> 요소를 문서에 추가
+        a.click();  												// 클릭해서 다운로드 시작
+        window.URL.revokeObjectURL(url);  							// URL 객체 해제
+        document.body.removeChild(a);  								// <a> 요소를 문서에서 제거
+    })
+    .catch(error => 
+    {
+        console.error('Error:', error);
+    });
+}
+
 

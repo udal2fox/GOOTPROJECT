965c94bc1ac2bdc3b088a2a55a47a338f7c196de
freetebet <id147147@naver.com>
2024-03-28 15:14:31 +0900

    orgChart

---
+++ src/main/java/org/rainbow/company/orgChart/domain/orgChartDTO.java
@@ -0,0 +1,28 @@
+package org.rainbow.company.orgChart.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class orgChartDTO {
+	
+	private int eno;
+	
+	private int deptNo;
+	
+	private String dName;
+	
+	private String eName;
+	
+	private String ePhone;
+	
+	private String email;
+	
+	private String job;
+	
+}
--- src/main/webapp/resources/js/company/login.js
+++ src/main/webapp/resources/js/company/login.js
@@ -13,8 +13,8 @@ document.head.appendChild(linkEle);
 const f = document.forms[0];
 const LOGIN_FAIL_MSG = '아이디 또는 비밀번호가 일치하지 않습니다';
 
-document.getElementById("loginBtn").addEventListener("click", () => {
- 
+//로그인 함수 정의
+function login() {
     if (!f.eId.value) {
         alert("아이디를 입력해주세요");
         f.eId.focus();
@@ -27,34 +27,44 @@ document.getElementById("loginBtn").addEventListener("click", () => {
     }
 
     fetch("/login", {
-		method : 'POST',
-		body : JSON.stringify({
-			eId : f.eId.value,
-			ePw : f.ePw.value,
-		}),
-		headers : {
-			"Content-type": "application/json; charset=UTF-8",
-		}
-	})
-	.then((Response) => {
-      if (Response.status == "400") {
-    	  alert(LOGIN_FAIL_MSG);
-    	  f.eId.focus();
-      }else {
-          Response.json().then((result) => {
-          sessionStorage.setItem("Okja", result.eno);
-          sessionStorage.setItem("deptNo", result.deptNo);
-          sessionStorage.setItem("eName", result.eName);
-          sessionStorage.setItem("profilePicture", result.profilePicture);
-          
-          location.href = "/moveSalesChart?" + sessionStorage.getItem("Okja");
-	})
-  }
-})	
-	.catch( err => {
-		// 에러 처리
-		console.log('Fetch error:', err);
-		// 에러 메시지 출력
-		console.log('Error message:', err.message);
-	});
+        method: 'POST',
+        body: JSON.stringify({
+            eId: f.eId.value,
+            ePw: f.ePw.value,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        }
+    })
+    .then((Response) => {
+        if (Response.status == "400") {
+            alert(LOGIN_FAIL_MSG);
+            f.eId.focus();
+        } else {
+            Response.json().then((result) => {
+                sessionStorage.setItem("Okja", result.eno);
+                sessionStorage.setItem("deptNo", result.deptNo);
+                sessionStorage.setItem("eName", result.eName);
+                sessionStorage.setItem("profilePicture", result.profilePicture);
+                
+                location.href = "/moveSalesChart?" + sessionStorage.getItem("Okja");
+            })
+        }
+    })  
+    .catch(err => {
+        // 에러 처리
+        console.log('Fetch error:', err);
+        // 에러 메시지 출력
+        console.log('Error message:', err.message);
+    });
+}
+
+// 로그인 버튼 클릭 시 로그인 함수 호출
+document.getElementById("loginBtn").addEventListener("click", login);
+
+// 비밀번호 입력란에서 엔터 키 눌렀을 때 로그인 함수 호출
+f.ePw.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        login();
+    }
 });
